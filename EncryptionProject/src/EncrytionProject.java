bae5f27c3536161c63b844bd5924da585e00eca9
GrilledWitch <emreotu.17@gmail.com>
2024-01-01 03:40:19 +0000

    EncrytionProject: Variables Declared

--- EncryptionProject/src/EncrytionProject.java
+++ EncryptionProject/src/EncrytionProject.java
@@ -1,7 +1,26 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
 
 public class EncrytionProject {
 	
-	public EncrytionProject() {}
+	private Scanner scanner;
+	private Random random;
+	private ArrayList<Character> list;
+	private ArrayList<Character> shuffledList;
+	private char character;
+	private String line;
+	private char[] letters;
+	private char[] secretLetters;
+
+	
+	public EncrytionProject() {
+		scanner = new Scanner(System.in);
+		random = new Random();
+		list = new ArrayList<Character>();
+		shuffledList = new ArrayList<Character>();
+		character = ' ';
+	}
 	
 	private void askQuestion() {}
 	
