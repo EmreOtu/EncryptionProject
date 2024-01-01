de82cc99240fe87491ca4dc29388433c7f087077
GrilledWitch <emreotu.17@gmail.com>
2024-01-01 03:35:13 +0000

    EncrytionProject: Important Methods Declared

--- EncryptionProject/src/EncrytionProject.java
+++ EncryptionProject/src/EncrytionProject.java
@@ -1,4 +1,17 @@
 
 public class EncrytionProject {
-
+	
+	public EncrytionProject() {}
+	
+	private void askQuestion() {}
+	
+	private void newKey() {}
+	
+	private void makeKey() {}
+	
+	private void encrypt() {}
+	
+	private void decrypt() {}
+	
+	private void quit() {}
 }
